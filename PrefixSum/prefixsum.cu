136c03cc046cc63dfbff3a90c20d3c4119721ae2
texashodlr <conorxdevlin@gmail.com>
2025-05-02 19:26:43 -0600

    Adding CUDA method

--- PrefixSum/prefixsum.cu
+++ PrefixSum/prefixsum.cu
@@ -9,9 +9,6 @@
 #include <random>
 #include <iostream>
 
-
-/*
-
 __global__ void parallel_scan(float* g_odata, float* g_idata, int n)
 {
 	extern __shared__ float temp[]; // allocated on invocation
@@ -35,7 +32,6 @@ __global__ void parallel_scan(float* g_odata, float* g_idata, int n)
 	g_odata[thid] = temp[pout * n + thid]; // write output
 }
 
-*/
 
 int sequential_scan(int* out, int* in, int n) {
 	memset(out, 0, sizeof(out));
@@ -49,6 +45,7 @@ int sequential_scan(int* out, int* in, int n) {
 		out[i] = out[i - 1] + in[i - 1];
 		printf("i = %d , Out = %d \n", i, out[i]);
 	}
+	return 0;
 
 }
 
@@ -56,8 +53,31 @@ int main() {
 	srand(time(NULL));
 	int n = 1 << 10;
 	int out[n], in[n];
-	
+
 	sequential_scan(out, in, n);
+
+	int* gpu_out, * gpu_in;
+	cudaMallocManaged(&gpu_out, n * sizeof(int));
+	cudaMallocManaged(&gpu_in, n * sizeof(int));
+
+	for (int i = 0; i < n;i++) {
+		gpu_out[i] = 0;
+		gpu_in[i] = rand() % 100;
+	}
+
+	int blockSize = 256;
+	int gridSize = (n + blockSize - 1) / blockSize;
+	parallel_scan << <gridSize, blockSize >> > (gpu_out, gpu_in, n);
+
+	cudaDeviceSynchronize();
+	
+	for (int i = 0; i < n;i++) {
+		printf("I: %d, Out: %d", i, gpu_out[i]);
+		
+	}
+
+	cudaFree(gpu_out);
+	cudaFree(gpu_in);
 
 	return 0;
 }
