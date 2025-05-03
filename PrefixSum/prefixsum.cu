2d73939f3645f6c5ec9eccdf97c7c304b25f6fe2
texashodlr <conorxdevlin@gmail.com>
2025-05-02 18:35:05 -0600

    Adding a simple method

--- PrefixSum/prefixsum.cu
+++ PrefixSum/prefixsum.cu
@@ -9,11 +9,38 @@
 #include <random>
 #include <iostream>
 
-int main() {
-	srand(time(NULL));
-	int k = 1 << 10;
-	int out[k], in[k];
+
+/*
+
+__global__ void parallel_scan(float* g_odata, float* g_idata, int n)
+{
+	extern __shared__ float temp[]; // allocated on invocation
+	int thid = threadIdx.x;
+	int pout = 0, pin = 1;
+	// Load input into shared memory.
+	// This is exclusive scan, so shift right by one
+	// and set first element to 0
+	temp[pout * n + thid] = (thid > 0) ? g_idata[thid - 1] : 0;
+	__syncthreads();
+	for (int offset = 1; offset < n; offset *= 2)
+	{
+		pout = 1 - pout; // swap double buffer indices
+		pin = 1 - pout;
+		if (thid > = offset)
+			temp[pout * n + thid] += temp[pin * n + thid - offset];
+		else
+			temp[pout * n + thid] = temp[pin * n + thid];
+		__syncthreads();
+	}
+	g_odata[thid] = temp[pout * n + thid]; // write output
+}
+
+*/
+
+int sequential_scan(int* g_odata, int* g_idata, int n) {
+	memset(in, 1, sizeof(in));
 	memset(out, 0, sizeof(out));
+
 	for (int i = 0; i < k;i++) {
 		in[i] = rand();
 	}
@@ -23,7 +50,17 @@ int main() {
 		out[i] = out[i - 1] + in[i - 1];
 		printf("i = %d , Out = %d \n", i, out[i]);
 	}
-	
 
+}
+
+int main() {
+	srand(time(NULL));
+	int n = 1 << 10;
+	int* out[n], in[n];
+	
+	sequential_scan(out, in, n);
+	
+	free(out);
+	free(in);
 	return 0;
 }
